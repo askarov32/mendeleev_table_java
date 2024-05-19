f24fe651d2f73fc946f8cd22793ee2038c257fca
askarov32 <insar@lessons.kz>
2024-05-19 21:59:08 +0500

    Modified HomeController and created Main file

--- src/main/java/main/mendeleyev/table_site/controllers/HomeController.java
+++ src/main/java/main/mendeleyev/table_site/controllers/HomeController.java
@@ -1,17 +1,31 @@
 package main.mendeleyev.table_site.controllers;
 
+import main.mendeleyev.table_site.db.DbManager;
+import main.mendeleyev.table_site.models.Element;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
     @GetMapping(value = "/")
-    public String mainPage(){
+    public String mainPage() {
         return "main";
     }
 
     @GetMapping(value = "/elements")
-    public String elementsPage(){
+    public String elementsPage(Model model) {
+        try {
+            ArrayList<Element> elements = DbManager.getAllElements();
+            model.addAttribute("elements", elements);
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
         return "elements";
     }
 }
